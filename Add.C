681a7a094cf2f7698c988bd6f019a8fb825eba16
AshishZone <AshishAbhilash@users.noreply.github.com>
2018-10-21 20:21:09 +0530

    Addition Prog.
    Simple two integers additon program.

---
+++ Add.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+ int integer1;
+ int integer2;
+ int sum;
+ 
+ printf("Enter the value of the first intger \n");
+ scanf("%d",&integer1);
+ 
+ printf("enter the value of the second integer \n");
+ scanf("%d",&integer2);
+ 
+ sum=integer1+integer2;
+ 
+ printf("The Sum is %d \n",sum);
+ return 0;
+}
