dcf00c574f6aab8d2c191f8120f9ff7a26d4cf27
AshishZone <AshishAbhilash@users.noreply.github.com>
2018-10-22 05:59:09 +0530

    Area of Triangle
    A simple C Prog. to find the area of the triangle given the base and the height of the triangle.

---
+++ TrianlgeArea.c
@@ -0,0 +1,10 @@
+#include<stdio.h.>
+int main()
+{
+int base,height,area;
+ printf("enter the values of the base and the height of the triangle \n");
+ scanf("%d %d",&base,&height);
+ area=(base*height)/2;
+ printf("the aea of the triangle is %d \n",area);
+ return 0;
+}
