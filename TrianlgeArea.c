932ae89c75f57455de14370980513fd586e69460
AshishZone <AshishAbhilash@users.noreply.github.com>
2018-10-25 16:27:35 +0530

    Update TrianlgeArea.c

--- TrianlgeArea.c
+++ TrianlgeArea.c
@@ -5,6 +5,6 @@ int base,height,area;
  printf("enter the values of the base and the height of the triangle \n");
  scanf("%d %d",&base,&height);
  area=(base*height)/2;
- printf("the aea of the triangle is %d \n",area);
+ printf("the area of the triangle is %d \n",area);
  return 0;
 }
